fa4aa3f4b652be94b651e9ab23624c04861c5df1
Garrett Cullen <garrettwcullen@gmail.com>
2017-03-22 10:16:30 -0700

    push

--- wp-content/themes/powers/footer.php
+++ wp-content/themes/powers/footer.php
@@ -14,9 +14,107 @@
 	
 	<div class="form"><?php gravity_form(1, false, false, false, '', true, 12); ?></div><!-- form -->
 	
+	<div class="lower_footer">
+		
+		<div class="footer_quote">
+			
+			<div class="footer_quote_content">
+				
+				
+			
+				<span class="top_quote">“While I know that my aggressiveness and education have won me many days in the courtroom, I also know that my value as a lawyer is the respect that I show each one of my clients on a daily basis.</span><!-- top_quote -->
+				
+				<span class="work_quote">
+					
+					<span class="footer_greenline"></span><!-- footer_greenline -->
+					
+					At Powers Injury Law, we are not just working for you, we are working with you.
+					
+				</span><!-- work_quote -->
+				
+				<span class="bottom_quote">This is a very personal process and as a result, it can be grueling. The relationship I have with my clients not only helps me better tell their story, but it makes the experience less stressful. ”</span><!-- bottom_quote -->
+				
+				<span class="footer_attorney">Attorney Jessica Powers</span><!-- footer_attorney -->
+			
+			</div><!-- footer_quote_content -->
+			
+		</div><!-- footer_quote -->
+		
+		
+		<div class="footer_contact">
+			
+			
+			<div class="footer_contact_wrapper">
+				
+				<div class="footer_contact_title_wrapper">
+					
+					<span class="footer_contact_title">Contact</span><!-- footer_contact_title -->
+					
+				</div><!-- footer_contact_title_wrapper -->
+				
+				<div class="footer_contact_info">
+					
+					
+					1234 Address Street<br/>Suite A<br/>Birmingham, Alabama<br/>01234
+					
+					<a class="get_directions" href="" target="_blank">Get Directions</a>
+					
+				</div><!-- footer_contact_info -->
+				
+			</div><!-- footer_contact_wrapper -->
+			
+			
+			<div class="footer_contact_wrapper">
+				
+				<div class="footer_contact_title_wrapper">
+					
+					<span class="footer_contact_title">Call</span><!-- footer_contact_title -->
+					
+				</div><!-- footer_contact_title_wrapper -->
+				
+				<div class="footer_contact_info">
+					
+					
+					<a class="tel" href="tel:1234567890">1234567890</a>
+					
+				
+					
+				</div><!-- footer_contact_info -->
+				
+			</div><!-- footer_contact_wrapper -->
+			
+			
+			
+			<div class="footer_contact_wrapper">
+				
+				<div class="footer_contact_title_wrapper">
+					
+					<span class="footer_contact_title">Connect</span><!-- footer_contact_title -->
+					
+				</div><!-- footer_contact_title_wrapper -->
+				
+				<div class="footer_contact_info">
+					
+					
+					<a class="social" href="" target="_blank">google+</a>
+					<a class="social" href="" target="_blank">facebook+</a>
+					
+				</div><!-- footer_contact_info -->
+				
+			</div><!-- footer_contact_wrapper -->
+			
+			
+			
+						
+			
+			
+		</div><!-- footer_contact -->
+		
+		
+		
+	</div><!-- lower_footer -->
+	
 </section><!-- request -->
-
-
 
 
 <?php wp_footer(); ?>
