a97a21c36ee5e2de154788cb6a3e53e5b72f87f6
Garrett Cullen <garrettwcullen@gmail.com>
2017-04-04 14:12:18 -0700

    push

--- wp-content/themes/powers/page-contact.php
+++ wp-content/themes/powers/page-contact.php
@@ -26,21 +26,21 @@ get_header(); ?>
 	</div><!-- green_header_wrapper -->
 	
 	
-	<div class="contact_wrapper">
+	<div class="contact_page_wrapper">
 		
 		<div class="map_wrapper">
 			
-			<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13398.12797532836!2d-117.113549!3d32.91054!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x48a46784ca309b0d!2sHigh+Rank+Websites%2C+Inc.!5e0!3m2!1sen!2sus!4v1491335071392" frameborder="0" style="border:0" allowfullscreen></iframe>
+		<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13398.12797532836!2d-117.113549!3d32.91054!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x48a46784ca309b0d!2sHigh+Rank+Websites%2C+Inc.!5e0!3m2!1sen!2sus!4v1491335071392" frameborder="0" style="border:0" allowfullscreen></iframe>
 			
 		</div><!-- map_wrapper -->
 		
-		<div class="contact_info_wrapper">
+		<div class="contact_page_info_wrapper">
 			
 			
 			
 		</div><!-- contact_info_wrapper -->
 		
-	</div><!-- contact_wrapper -->
+	</div><!-- contact_page_wrapper -->
 	
 	
 	
