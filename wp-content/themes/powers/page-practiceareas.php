41757874ee788da6a8636f707363f3b1d38072eb
Garrett Cullen <garrettwcullen@gmail.com>
2017-04-04 10:46:33 -0700

    push

---
+++ wp-content/themes/powers/page-practiceareas.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Template Name: Practice Areas Directory
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Ten
+ * @since Twenty Ten 1.0
+ */
+
+get_header(); ?>
+
+<?php include('banner.php');?>
+
+<section class="inner_main">
+	
+	<h1 class="centered">Practice Areas</h1>
+	<div class="green_header_wrapper">
+		
+		<div class="green_header"></div><!-- green_header -->
+		
+	</div><!-- green_header_wrapper -->
+	
+	
+	<div class="practice_areas_wrapper">
+		
+		<ul>
+			
+			<li><a class="" href="">Practice Area</a></li>
+			<li><a class="" href="">Practice Area</a></li>
+			<li><a class="" href="">Practice Area</a></li>
+			<li><a class="" href="">Practice Area</a></li>
+			<li><a class="" href="">Practice Area</a></li>
+			<li><a class="" href="">Practice Area</a></li>
+			
+			<li><a class="" href="">Practice Area</a></li>
+			<li><a class="" href="">Practice Area</a></li>
+			<li><a class="" href="">Practice Area</a></li>
+			<li><a class="" href="">Practice Area</a></li>
+			<li><a class="" href="">Practice Area</a></li>
+			<li><a class="" href="">Practice Area</a></li>
+			
+			<li><a class="" href="">Practice Area</a></li>
+			<li><a class="" href="">Practice Area</a></li>
+			<li><a class="" href="">Practice Area</a></li>
+			<li><a class="" href="">Practice Area</a></li>
+			<li><a class="" href="">Practice Area</a></li>
+			<li><a class="" href="">Practice Area</a></li>
+			
+		</ul>
+		
+	</div><!-- practice_areas_wrapper -->
+	
+	
+	
+</section><!-- inner_main -->
+
+
+
+		
+
+
+
+
+<?php get_footer(); ?>
