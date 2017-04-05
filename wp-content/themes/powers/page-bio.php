920d01474ccc8fcf22e3e0542c036caaf77568b4
Garrett Cullen <garrettwcullen@gmail.com>
2017-04-05 11:13:39 -0700

    push

--- wp-content/themes/powers/page-bio.php
+++ wp-content/themes/powers/page-bio.php
@@ -20,13 +20,49 @@ get_header(); ?>
 	
 	<div class="our_attorney_wrapper">
 		
-		<div class="our_attorney_image">
+		<div class="our_attorney_inner_wrapper">
+		
+			<div class="our_attorney_image">
 			
-			<img src="<?php bloginfo('template_directory');?>/images/profile-powers.jpg"/>
+				<img src="<?php bloginfo('template_directory');?>/images/profile-powers.jpg"/>
 			
-		</div><!-- our_attorney_image -->
+			</div><!-- our_attorney_image -->
+		
+			<div class="our_attorney_content">
+			
+				<span class="attorney_title">Attorney Jessica Powers</span><!-- attorney_title -->
+			
+				<span class="attorney_quote"><span class="green">I advocate</span> on behalf of individuals, <span class="green">for all people...to provide them a voice</span> in a very loud and confusing system.</span><!-- attorney_quote -->
+			
+			</div><!-- our_attorney_content -->
+		
+			</div><!-- our_attorney_inner_wrapper -->
 		
 	</div><!-- our_attorney_wrapper -->
+	
+	<div class="our_attorney_columns">
+		
+		<div class="content">
+		
+			<div class="att_col">
+				
+				<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+
+				<h2>Heading style 2 - Nemo enim ipsam voluptatem</h2> 
+<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+<p>ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
+
+
+
+				
+			</div><!-- att_col -->
+		
+			<div class="att_col"></div><!-- att_col -->
+		
+		</div><!-- content -->
+		
+	</div><!-- our_attorney_columns -->
 		
 	
 	
