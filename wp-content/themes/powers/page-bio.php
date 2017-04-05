c384dd2be0eec9ca1f05747a1e3bb8e4ca26a881
Garrett Cullen <garrettwcullen@gmail.com>
2017-04-05 10:42:27 -0700

    push

---
+++ wp-content/themes/powers/page-bio.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Template Name: Meet Our Attorney
+ *
+
+ */
+
+get_header(); ?>
+
+<?php include('banner.php');?>
+
+<section class="inner_main">
+	
+	<h1 class="centered">Meet Our Attorney</h1>
+	<div class="green_header_wrapper">
+		
+		<div class="green_header"></div><!-- green_header -->
+		
+	</div><!-- green_header_wrapper -->
+	
+	<div class="our_attorney_wrapper">
+		
+		<div class="our_attorney_image">
+			
+			<img src="<?php bloginfo('template_directory');?>/images/profile-powers.jpg"/>
+			
+		</div><!-- our_attorney_image -->
+		
+	</div><!-- our_attorney_wrapper -->
+		
+	
+	
+</section><!-- inner_main -->
+
+
+
+		
+
+
+
+
+<?php get_footer(); ?>
--- wp-content/themes/powers/page-contact.php
+++ wp-content/themes/powers/page-contact.php
@@ -2,14 +2,7 @@
 /**
  * Template Name: Contact
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
- *
- * @package WordPress
- * @subpackage Twenty_Ten
- * @since Twenty Ten 1.0
+ 
  */
 
 get_header(); ?>
