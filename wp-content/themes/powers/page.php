eba4b00a466955e0a8b80c96d14444ca809529c7
Garrett Cullen <garrettwcullen@gmail.com>
2017-04-06 14:58:24 -0700

    push

--- wp-content/themes/powers/page.php
+++ wp-content/themes/powers/page.php
@@ -24,38 +24,42 @@ get_header(); ?>
 		
 		<div class="content_inner_wrapper">
 			
-			<div class="header_wrapper">
+			<div class="content_max_width">
+			
+				<div class="header_wrapper">
 				
-				<span class="inner_page_green_line"></span><!-- inner_page_green_line -->
+					<span class="inner_page_green_line"></span><!-- inner_page_green_line -->
 			
-				<h1 class="standard_page_header">Birmingham Car Accident Attorneys</h1>
+					<h1 class="standard_page_header">Birmingham Car Accident Attorneys</h1>
 			
-			</div><!-- header_wrapper -->
+				</div><!-- header_wrapper -->
 			
 			
 			
-			<div class="content">
+				<div class="content">
 				
 				
-				<p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur autodit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+					<p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur autodit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 
-				<h2>Heading style 2 - Nemo enim ipsam voluptatem</h2>
+					<h2>Heading style 2 - Nemo enim ipsam voluptatem</h2>
 				
-				<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. <a href="">Embedded link style</a> cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. <a href="">Embedded link style</a> cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
-				<h3>Heading Style 3 - Nemo enim ipsam voluptatem</h3>
+					<h3>Heading Style 3 - Nemo enim ipsam voluptatem</h3>
 Nemo enim ipsam voluptatem quia voluptas sit aspernatur autodit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
 
-				<ul>
+					<ul>
 
-					<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, se consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li> 
-					<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li> 
-					<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li> 
+						<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, se consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li> 
+						<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li> 
+						<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li> 
 
-				</ul>
+					</ul>
 					
 			
-			</div><!-- content -->
+				</div><!-- content -->
+			
+			</div><!-- content_max_width -->
 			
 			
 		</div><!-- content_inner_wrapper -->
