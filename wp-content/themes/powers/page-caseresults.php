1764238f52e6e8e35a5cb1bd57d3578b89fde1ea
Garrett Cullen <garrettwcullen@gmail.com>
2017-04-07 13:42:23 -0700

    push

--- wp-content/themes/powers/page-caseresults.php
+++ wp-content/themes/powers/page-caseresults.php
@@ -22,6 +22,71 @@ get_header(); ?>
 	
 	<div class="case_results_page_wrapper">
 		
+		<div class="case_column">
+		
+			<div class="single_case">
+			
+			<span class="case_amount">$1.2M</span><!--case_amount -->
+			<span class="case_title">Wrongful Death</span><!-- case_title -->
+			
+			<div class="case_content">
+				
+				<span class="case_greenline"></span><!-- case_greenline -->
+				
+				<div class="case_content_wrapper">
+				
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque luctus, dolor a convallis efficitur, odio ante sollicitudin ipsum, eu venenatis eros orci at tellus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque luctus odio.</p>
+				
+				</div><!-- case_content_wrapper -->
+				
+			</div><!-- case_content -->
+			
+			
+			<div class="case_cat_mobile_wrapper">
+			
+				<div class="case_cat">
+					Settlement
+				</div><!-- case_cat -->
+			
+			</div><!-- case_cat_mobile_wrapper -->
+			
+		</div><!-- single_case -->
+		
+		
+			<div class="single_case">
+			
+			<span class="case_amount">$1.2M</span><!--case_amount -->
+			<span class="case_title">Wrongful Death</span><!-- case_title -->
+			
+			<div class="case_content">
+				
+				<span class="case_greenline"></span><!-- case_greenline -->
+				
+				<div class="case_content_wrapper">
+				
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque luctus, dolor a convallis efficitur, odio ante sollicitudin ipsum, eu venenatis eros orci at tellus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque luctus odio.</p>
+				
+				</div><!-- case_content_wrapper -->
+				
+			</div><!-- case_content -->
+			
+			
+			<div class="case_cat_mobile_wrapper">
+			
+				<div class="case_cat">
+					Settlement
+				</div><!-- case_cat -->
+			
+			</div><!-- case_cat_mobile_wrapper -->
+			
+		</div><!-- single_case -->
+		
+		
+		</div><!-- case_column -->
+		
+		<div class="case_column">
+		
+		
 		<div class="single_case">
 			
 			<span class="case_amount">$1.2M</span><!--case_amount -->
@@ -39,11 +104,52 @@ get_header(); ?>
 				
 			</div><!-- case_content -->
 			
-			<div class="case_cat">
-				Settlement
-			</div><!-- case_cat -->
+			
+			<div class="case_cat_mobile_wrapper">
+			
+				<div class="case_cat">
+					Settlement
+				</div><!-- case_cat -->
+			
+			</div><!-- case_cat_mobile_wrapper -->
 			
 		</div><!-- single_case -->
+		
+		
+		
+		<div class="single_case">
+			
+			<span class="case_amount">$1.2M</span><!--case_amount -->
+			<span class="case_title">Wrongful Death</span><!-- case_title -->
+			
+			<div class="case_content">
+				
+				<span class="case_greenline"></span><!-- case_greenline -->
+				
+				<div class="case_content_wrapper">
+				
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque luctus, dolor a convallis efficitur, odio ante sollicitudin ipsum, eu venenatis eros orci at tellus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque luctus odio.</p>
+				
+				</div><!-- case_content_wrapper -->
+				
+			</div><!-- case_content -->
+			
+			
+			<div class="case_cat_mobile_wrapper">
+			
+				<div class="case_cat">
+					Settlement
+				</div><!-- case_cat -->
+			
+			</div><!-- case_cat_mobile_wrapper -->
+			
+		</div><!-- single_case -->
+		
+		</div><!-- case_column -->
+		
+		
+		<div style="clear:both"></div>
+		
 		
 	</div><!-- case_results_wrapper -->
 		
