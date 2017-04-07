c8f9fae4a6b3f94a5f4a79274d3caaf4a3cec6f7
Garrett Cullen <garrettwcullen@gmail.com>
2017-04-07 10:00:41 -0700

    push

---
+++ wp-content/themes/powers/page-caseresults.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Template Name: Case Results
+ *
+
+ */
+
+get_header(); ?>
+
+<?php include('banner.php');?>
+
+<section class="inner_main">
+	
+	<h1 class="centered">Case Results</h1>
+	<div class="green_header_wrapper">
+		
+		<div class="green_header"></div><!-- green_header -->
+		
+	</div><!-- green_header_wrapper -->
+	
+	
+	
+	<div class="case_results_page_wrapper">
+		
+		<div class="single_case">
+			
+			<span class="case_amount">$1.2M</span><!--case_amount -->
+			<span class="case_title">Wrongful Death</span><!-- case_title -->
+			
+			<div class="case_content">
+				
+				<span class="case_greenline"></span><!-- case_greenline -->
+				
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque luctus, dolor a convallis efficitur, odio ante sollicitudin ipsum, eu venenatis eros orci at tellus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque luctus odio.</p>
+				
+			</div><!-- case_content -->
+			
+		</div><!-- single_case -->
+		
+	</div><!-- case_results_wrapper -->
+		
+	
+	
+</section><!-- inner_main -->
+
+
+
+		
+
+
+
+
+<?php get_footer(); ?>
