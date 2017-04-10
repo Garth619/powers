7600b24f1c129b56130c124d6030acd034384036
Garrett Cullen <garrettcullen@garretts-MacBook-Pro.local>
2017-04-09 19:57:33 -0700

    push

---
+++ wp-content/themes/powers/page-testimonials.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * Template Name: Testimonials
+ *
+
+ */
+
+get_header(); ?>
+
+<?php include('banner.php');?>
+
+<section class="inner_main">
+	
+	<h1 class="centered">Client Testimonials</h1>
+	<div class="green_header_wrapper">
+		
+		<div class="green_header"></div><!-- green_header -->
+		
+	</div><!-- green_header_wrapper -->
+	
+	
+	<div class="testimonials_page_wrapper">
+		
+		
+		<div class="single_testimonial">
+			
+			<div class="testimonial_header_wrapper">
+				
+				<span class="testi_greenline"></span><!-- testi_greenline -->
+				
+				<span class="testi_header">Jessica Powers is the attorney that you can trust</span><!-- testi_header -->
+				
+								
+			</div><!-- testimonial_header_wrapper -->
+			
+			<p>“With feelings of hopelessness and confusion, I made one phone call that would turn all of that around. Jessica has given me answers and hope from the first phone call, treating me like a family member not a client. Any questions or problems I had I was not waiting days on answers, Jessica was getting problems fixed and answers within hours of me calling or e-mailing. I have no worries and know that everything is being taken care of on time and for my best interest because I have Jessica on my side! Jessica Powers is the attorney that you can trust and is truly working for you to help you in these hard and confusing times.”</p>
+
+			<span class="name">Andi K.<span class="divider"></span> Current Client</span><!-- name -->
+			
+		</div><!-- single_testimonial -->
+		
+		
+		<div class="single_testimonial">
+			
+			<div class="testimonial_header_wrapper">
+				
+				<span class="testi_greenline"></span><!-- testi_greenline -->
+				
+				<span class="testi_header">Jessica Powers is the attorney that you can trust</span><!-- testi_header -->
+				
+								
+			</div><!-- testimonial_header_wrapper -->
+			
+			<p>“With feelings of hopelessness and confusion, I made one phone call that would turn all of that around. Jessica has given me answers and hope from the first phone call, treating me like a family member not a client. Any questions or problems I had I was not waiting days on answers, Jessica was getting problems fixed and answers within hours of me calling or e-mailing. I have no worries and know that everything is being taken care of on time and for my best interest because I have Jessica on my side! Jessica Powers is the attorney that you can trust and is truly working for you to help you in these hard and confusing times.”</p>
+
+			<span class="name">Andi K.<span class="divider"></span> Current Client</span><!-- name -->
+			
+		</div><!-- single_testimonial -->
+		
+		
+		<div class="single_testimonial">
+			
+			<div class="testimonial_header_wrapper">
+				
+				<span class="testi_greenline"></span><!-- testi_greenline -->
+				
+				<span class="testi_header">Jessica Powers is the attorney that you can trust</span><!-- testi_header -->
+				
+								
+			</div><!-- testimonial_header_wrapper -->
+			
+			<p>“With feelings of hopelessness and confusion, I made one phone call that would turn all of that around. Jessica has given me answers and hope from the first phone call, treating me like a family member not a client. Any questions or problems I had I was not waiting days on answers, Jessica was getting problems fixed and answers within hours of me calling or e-mailing. I have no worries and know that everything is being taken care of on time and for my best interest because I have Jessica on my side! Jessica Powers is the attorney that you can trust and is truly working for you to help you in these hard and confusing times.”</p>
+
+			<span class="name">Andi K.<span class="divider"></span> Current Client</span><!-- name -->
+			
+		</div><!-- single_testimonial -->
+		
+		
+		<div class="single_testimonial">
+			
+			<div class="testimonial_header_wrapper">
+				
+				<span class="testi_greenline"></span><!-- testi_greenline -->
+				
+				<span class="testi_header">Jessica Powers is the attorney that you can trust</span><!-- testi_header -->
+				
+								
+			</div><!-- testimonial_header_wrapper -->
+			
+			<p>“With feelings of hopelessness and confusion, I made one phone call that would turn all of that around. Jessica has given me answers and hope from the first phone call, treating me like a family member not a client. Any questions or problems I had I was not waiting days on answers, Jessica was getting problems fixed and answers within hours of me calling or e-mailing. I have no worries and know that everything is being taken care of on time and for my best interest because I have Jessica on my side! Jessica Powers is the attorney that you can trust and is truly working for you to help you in these hard and confusing times.”</p>
+
+			<span class="name">Andi K.<span class="divider"></span> Current Client</span><!-- name -->
+			
+		</div><!-- single_testimonial -->
+		
+		<div style="clear:both"></div>
+		
+	
+	</div><!-- testimonials_wrapper -->
+			
+	
+	
+</section><!-- inner_main -->
+
+
+
+		
+
+
+
+
+<?php get_footer(); ?>
