24804d7f51826f9d66280ec14928b2074afad903
Garrett Cullen <garrettcullen@garretts-MacBook-Pro.local>
2017-04-09 21:04:48 -0700

    push

--- wp-content/themes/powers/page-blog.php
+++ wp-content/themes/powers/page-blog.php
@@ -1,21 +1,13 @@
 <?php
 /**
- * Main template file
+ * Template Name: Blog Page
  *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- * Learn more: https://codex.wordpress.org/Template_Hierarchy
- *
- * @package WordPress
- * @subpackage Twenty_Ten
- * @since Twenty Ten 1.0
- */
+*/
 
 get_header(); ?>
 
-		
+
+	
 
 <?php include('banner.php');?>
 
@@ -73,4 +65,5 @@ get_header(); ?>
 	
 
 <?php get_footer(); ?>
+
 
