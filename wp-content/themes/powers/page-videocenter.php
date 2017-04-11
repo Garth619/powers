22b4d0c8614243344b13f7716c1739205174c2eb
Garrett Cullen <garrettwcullen@gmail.com>
2017-04-11 10:05:04 -0700

    push

--- wp-content/themes/powers/page-videocenter.php
+++ wp-content/themes/powers/page-videocenter.php
@@ -48,7 +48,7 @@ get_header(); ?>
 			<div class="video_center_title_wrapper">
 				
 				<span class="video_center_greenline"></span><!-- video_center_greenline -->
-				<span class="video_center_title">Video Title Here, Two Lines or less</span><!-- video_center_title -->
+				<span class="video_center_title">Firm Overview</span><!-- video_center_title -->
 				
 			</div><!-- video_center_title_wrapper -->
 			
@@ -62,7 +62,7 @@ get_header(); ?>
 				
 				<div class="wistia_video">
 					
-					<script src="https://fast.wistia.com/embed/medias/v2q97a6dh4.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><span class="wistia_embed wistia_async_v2q97a6dh4 popover=true popoverAnimateThumbnail=true videoFoam=true" style="display:inline-block;height:100%;width:100%">&nbsp;</span></div></div>
+					<script src="https://fast.wistia.com/embed/medias/7vphf52t5a.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><span class="wistia_embed wistia_async_7vphf52t5a popover=true popoverAnimateThumbnail=true videoFoam=true" style="display:inline-block;height:100%;width:100%">&nbsp;</span></div></div>
 					
 					<div class="video_center_play_button">
 						
@@ -82,7 +82,7 @@ get_header(); ?>
 			<div class="video_center_title_wrapper">
 				
 				<span class="video_center_greenline"></span><!-- video_center_greenline -->
-				<span class="video_center_title">Video Title Here, Two Lines or less</span><!-- video_center_title -->
+				<span class="video_center_title">Worker's Compensation</span><!-- video_center_title -->
 				
 			</div><!-- video_center_title_wrapper -->
 			
@@ -96,7 +96,7 @@ get_header(); ?>
 				
 				<div class="wistia_video">
 					
-					<script src="https://fast.wistia.com/embed/medias/v2q97a6dh4.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><span class="wistia_embed wistia_async_v2q97a6dh4 popover=true popoverAnimateThumbnail=true videoFoam=true" style="display:inline-block;height:100%;width:100%">&nbsp;</span></div></div>
+					<script src="https://fast.wistia.com/embed/medias/6fuc36qc9c.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><span class="wistia_embed wistia_async_6fuc36qc9c popover=true popoverAnimateThumbnail=true videoFoam=true" style="display:inline-block;height:100%;width:100%">&nbsp;</span></div></div>
 					
 					<div class="video_center_play_button">
 						
@@ -116,7 +116,7 @@ get_header(); ?>
 			<div class="video_center_title_wrapper">
 				
 				<span class="video_center_greenline"></span><!-- video_center_greenline -->
-				<span class="video_center_title">Video Title Here, Two Lines or less</span><!-- video_center_title -->
+				<span class="video_center_title">Premises Liability </span><!-- video_center_title -->
 				
 			</div><!-- video_center_title_wrapper -->
 			
@@ -130,7 +130,7 @@ get_header(); ?>
 				
 				<div class="wistia_video">
 					
-					<script src="https://fast.wistia.com/embed/medias/v2q97a6dh4.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><span class="wistia_embed wistia_async_v2q97a6dh4 popover=true popoverAnimateThumbnail=true videoFoam=true" style="display:inline-block;height:100%;width:100%">&nbsp;</span></div></div>
+					<script src="https://fast.wistia.com/embed/medias/kwpxqk4lv1.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><span class="wistia_embed wistia_async_kwpxqk4lv1 popover=true popoverAnimateThumbnail=true videoFoam=true" style="display:inline-block;height:100%;width:100%">&nbsp;</span></div></div>
 					
 					<div class="video_center_play_button">
 						
@@ -150,7 +150,7 @@ get_header(); ?>
 			<div class="video_center_title_wrapper">
 				
 				<span class="video_center_greenline"></span><!-- video_center_greenline -->
-				<span class="video_center_title">Video Title Here, Two Lines or less</span><!-- video_center_title -->
+				<span class="video_center_title">Truck Accidents</span><!-- video_center_title -->
 				
 			</div><!-- video_center_title_wrapper -->
 			
@@ -164,7 +164,7 @@ get_header(); ?>
 				
 				<div class="wistia_video">
 					
-					<script src="https://fast.wistia.com/embed/medias/v2q97a6dh4.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><span class="wistia_embed wistia_async_v2q97a6dh4 popover=true popoverAnimateThumbnail=true videoFoam=true" style="display:inline-block;height:100%;width:100%">&nbsp;</span></div></div>
+					<script src="https://fast.wistia.com/embed/medias/ufvs667ynw.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><span class="wistia_embed wistia_async_ufvs667ynw popover=true popoverAnimateThumbnail=true videoFoam=true" style="display:inline-block;height:100%;width:100%">&nbsp;</span></div></div>
 					
 					<div class="video_center_play_button">
 						
@@ -184,7 +184,7 @@ get_header(); ?>
 			<div class="video_center_title_wrapper">
 				
 				<span class="video_center_greenline"></span><!-- video_center_greenline -->
-				<span class="video_center_title">Video Title Here, Two Lines or less</span><!-- video_center_title -->
+				<span class="video_center_title">Wrongful Death</span><!-- video_center_title -->
 				
 			</div><!-- video_center_title_wrapper -->
 			
@@ -198,7 +198,7 @@ get_header(); ?>
 				
 				<div class="wistia_video">
 					
-					<script src="https://fast.wistia.com/embed/medias/v2q97a6dh4.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><span class="wistia_embed wistia_async_v2q97a6dh4 popover=true popoverAnimateThumbnail=true videoFoam=true" style="display:inline-block;height:100%;width:100%">&nbsp;</span></div></div>
+					<script src="https://fast.wistia.com/embed/medias/w15tmuka1i.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><span class="wistia_embed wistia_async_w15tmuka1i popover=true popoverAnimateThumbnail=true videoFoam=true" style="display:inline-block;height:100%;width:100%">&nbsp;</span></div></div>
 					
 					<div class="video_center_play_button">
 						
@@ -218,11 +218,51 @@ get_header(); ?>
 			<div class="video_center_title_wrapper">
 				
 				<span class="video_center_greenline"></span><!-- video_center_greenline -->
-				<span class="video_center_title">Video Title Here, Two Lines or less</span><!-- video_center_title -->
+				<span class="video_center_title">Distracted Driving</span><!-- video_center_title -->
 				
 			</div><!-- video_center_title_wrapper -->
 			
 		</div><!-- single_video_wrapper -->
+		
+		
+		
+		<div class="single_video_wrapper">
+			
+			<div class="video_background">
+				
+				<div class="wistia_video">
+					
+					<script src="https://fast.wistia.com/embed/medias/sxi3o4n4ur.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><span class="wistia_embed wistia_async_sxi3o4n4ur popover=true popoverAnimateThumbnail=true videoFoam=true" style="display:inline-block;height:100%;width:100%">&nbsp;</span></div></div>
+					
+					<div class="video_center_play_button">
+						
+						<div class="video_center_play_button_inner">
+						
+						<img class="video_page_play" src="<?php bloginfo('template_directory');?>/images/playbutton.png"/>
+						<img class="video_page_play_hover" src="<?php bloginfo('template_directory');?>/images/playbutton-h.png"/>
+						
+						</div><!-- video_center_play_button_inner -->
+						
+					</div><!-- video_center_play_button -->
+					
+				</div><!-- wistia_video -->
+				
+			</div><!-- video_background -->
+			
+			<div class="video_center_title_wrapper">
+				
+				<span class="video_center_greenline"></span><!-- video_center_greenline -->
+				<span class="video_center_title">Car Accidents</span><!-- video_center_title -->
+				
+			</div><!-- video_center_title_wrapper -->
+			
+		</div><!-- single_video_wrapper -->
+		
+		
+		
+		
+		
+		
 		
 		
 	</div><!-- video_center_wrapper -->
